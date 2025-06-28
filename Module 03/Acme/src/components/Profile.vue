4965a56e68884b6b74a6f055ef521d67e8b6e6ec
Joel Mussman <jmussman@smallrock.net>
2025-06-27 21:35:41 -0400

    Module 3 in progress

---
+++ Module 03/Acme/src/components/Profile.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true,
+    },
+})
+</script>
+
+<template>
+    <h1>User Profile from Auth0</h1>
+</template>
+
+<style scoped></style>
